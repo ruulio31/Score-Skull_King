829b531a79dd8b3bbfb1736f7a0925b4fd90ad11
Julien PC portable <julien.desveaux@gmail.com>
2019-04-23 19:00:21 +0200

    reformat code catch the exception negative value for the gamble attr of player

--- scoring.py
+++ scoring.py
@@ -8,64 +8,56 @@ class Player:
         self.score = 0
         self.name = input("Entrez le nom du joueur:")
         # FIXME : lock the access to only the 10 values rounds[1 -> 10]
-        self.gamble = [None for i in range(1,12)]
+        self.gamble = [None for i in range(1, 12)]
 
     def set_gamble(self, round):
         try:
-            self.gamble[round] = int(input(f"Player {self.name}: "))
-        except ValueError:
-            print("Not a number entered")
-            
+            gamble = int(input(f"Player {self.name}: "))
+            if gamble < 0:
+                raise ValueError("Enter a postive number")
+            self.gamble[round] = gamble
+        except ValueError as exp:
+            print(exp.args[0])
+            #FIXME Let the user enter again the value, recall set_gamble and input an exit value?
+
+
+
     def set_score(self):
         try:
-            self.score =+ int(input(f"Entrez score du joueur {self.name}: "))
+            self.score += int(input(f"Entrez score du joueur {self.name}: "))
         except ValueError:
             print("Not a number entered")
-            
-    
 
 
 def player_creation():
     player_list_creation = True
     players = []
-    while player_list_creation :
+    while player_list_creation:
         players.append(Player())
         if int(input("Entrez 0 pour terminer la saise des joueurs, 1 pour continuer")) == 0:
             player_list_creation = False
         else:
             player_list_creation = True
-            
     return players
-
-
 
 
 def main():
     # Game start : create player
     players = player_creation()
 
-
-    for round in range(1,3):
+    for round in range(1, 3):
         # Start gambling
-        #FIXME the android won't use a loop but a clic button
+        # FIXME the android won't use a loop but a clic button
         print(f"Round {round}", "Gambling !!!")
-        for player in players: player.set_gamble(round)                
+        for player in players: player.set_gamble(round)
 
         # Scoring
         for player in players:
-            player.set_score()                      
+            player.set_score()
 
     for player in players:
         print(f"fin de partie, score : \n {player.name} : {player.score}")
 
 
-
-
-
-
-
-
-
 if __name__ == '__main__':
     main()
-
